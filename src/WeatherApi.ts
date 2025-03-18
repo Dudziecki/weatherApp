f4b1ba427a155876448bebd44a813735aaf5b402
Michael Dudzik <mkdudik15@gmail.com>
2025-03-18 21:26:48 +0300

    non use instance and weatherApi

--- src/WeatherApi.ts
+++ src/WeatherApi.ts
@@ -1,6 +1,40 @@
-import axios from "axios";
+import { instance } from "./instance.ts";
 
-const getWeather=async ()=>{
-    const response=axios.get("https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&hourly=temperature_2m&forecast_days=1")
-    console.log(response)
-}
+ const weatherApi = {
+    // Получаем координаты города
+    async getCoordinates(city: string) {
+        try {
+            debugger
+            const geoRes = await instance.get("https://geocoding-api.open-meteo.com/v1/search", {
+                params: { name: city, count: 1, language: "en", format: "json" }
+            });
+
+            if (!geoRes.data.results || geoRes.data.results.length === 0) {
+                throw new Error("Город не найден");
+
+            }
+
+            return geoRes.data.results[0]; // Возвращаем объект с latitude и longitude
+        } catch (error) {
+            throw new Error("Ошибка при получении координат");
+        }
+    },
+
+    // Получаем прогноз погоды по координатам
+    async getWeather(latitude: number, longitude: number) {
+        try {
+            const weatherRes = await instance.get("", {
+                params: {
+                    latitude,
+                    longitude,
+                    hourly: "temperature_2m",
+                    forecast_days: 1
+                }
+            });
+
+            return weatherRes.data; // Возвращаем данные о погоде
+        } catch (error) {
+            throw new Error("Ошибка при получении прогноза погоды");
+        }
+    }
+};
