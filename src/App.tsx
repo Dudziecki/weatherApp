72e2ad06f6f2ff4221922738226c01ce01546611
Michael Dudzik <mkdudik15@gmail.com>
2025-03-18 21:25:28 +0300

    add local state and some handlers

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,38 @@
 import './App.css'
-import {Weather} from "./Weather.tsx";
+import { Weather } from "./Weather.tsx";
+import { useDispatch } from "react-redux";
+import { getWeatherDataTC } from "./weatherSlice.ts";
+import { AppDispatch } from "./store/store.ts";
+import { useState } from "react";
 
 function App() {
-  return (
-      <div className="app">
-        <div>
-            <input type="text" placeholder="Enter City" />
-            <button>search</button>
-            <Weather/>
+    const dispatch = useDispatch<AppDispatch>();
+    const [city, setCity] = useState("");
+
+    const changeHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setCity(e.target.value);
+    };
+
+    const clickHandler = () => {
+        if (city.trim()) {
+            dispatch(getWeatherDataTC(city)); // Передаем город в санку
+        }
+    };
+
+    return (
+        <div className="app">
+            <div>
+                <input
+                    type="text"
+                    placeholder="Enter City"
+                    value={city}
+                    onChange={changeHandler}
+                />
+                <button onClick={clickHandler}>Search</button>
+                <Weather/>
+            </div>
         </div>
-      </div>
-  )
+    );
 }
 
-export default App
+export default App;
